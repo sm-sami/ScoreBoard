f44281420c0cffb585d85c43c0a5abd7eebd0020
Mohamed Sami <srmf2025@gmail.com>
2022-05-07 18:57:18 +0530

    add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let buttonContent = ''
+</script>
+
+{#if buttonContent}
+    <button on:click>{buttonContent}</button>
+{/if}
+
+<style>
+    button {
+        padding: 7px 10px;
+        margin: 5px;
+        border-radius: 10px;
+        border: none;
+        background-color: var(--color-secondary);
+        color: var(--text-color);
+        font-weight: bold;
+    }
+</style>
