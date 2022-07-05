681a155ff23dce5aae44027cc0a9fc420d86ad42
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-07-05 18:10:09 +0530

    :sparkles: add Topbar component

---
+++ src/components/Topbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { AddPlayer, Button } from "./";
+    import { PlayerStore } from "../stores";
+
+    const resetStore = () => {
+        PlayerStore.update(() => []);
+    }
+</script>
+
+<nav>
+    <AddPlayer />
+    <Button buttonContent="New Game" on:click={resetStore}/>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        padding: 20px 10px 0;
+        justify-content: space-between;
+    }
+    
+    @media (min-width: 768px) {
+        nav {
+            padding: 20px 50px 0;
+        }
+    }
+</style>
