e19c744bf2aad1af3825304f26086c22be7119c1
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-07-06 18:52:35 +0530

    :sparkles: save store to local storage

--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,7 @@
 import { writable } from "svelte/store";
 
-export const PlayerStore = writable([]);
+const storedPlayers = JSON.parse(localStorage.getItem("ScoreBoard.players"));
+
+export const PlayerStore = writable(storedPlayers ?? []);
+
+PlayerStore.subscribe((players) => localStorage.setItem("ScoreBoard.players", JSON.stringify(players)));
